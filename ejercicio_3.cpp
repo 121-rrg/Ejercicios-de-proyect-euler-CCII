74c1d983274fe74b7f28cb5f418e5bc1d227274e
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-04-24 18:27:22 -0500

    Update ejercicio_3.cpp

--- ejercicio_3.cpp
+++ ejercicio_3.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
 using namespace std;
+
 int main(){
-    long long n=1,num=600851475143;
-    while(n!=num){
-        if(num%n==0)
-            num=num/n;
-        n+=1;
+    long long n=1,num=600851475143; // declaramos nuestras variables 
+    while(n < num){ // mientras n sea menor num
+        if(num%n==0 && n != num)  // condicion si  num es multiplo de n  y cuando sean diferentes
+            num=num/n; //se divide el numero entre el numero de veces que haya recorrido el ciclo osea n
+        n++; // aumentamos n cada que no cumpla la condicion
     }
-    cout<<"El resultado es "<<num;
+    cout<<"El resultado es "<<num; // imprimimos el valor en pantalla
     return 0;
 }
