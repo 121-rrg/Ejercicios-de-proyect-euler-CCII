f9b37d89f0251ac35c42b479af92eccc0c742cd1
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-04-24 18:27:04 -0500

    Update ejercicio_2.cpp

--- ejercicio_2.cpp
+++ ejercicio_2.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    int a=1,b=2,aux=0,suma=0;
-    while(b<=4000000){
-        aux=a+b;
-        a=b;
-        b=aux;
-        if(a%2==0)
-            suma+=a;
+    int a=1,b=2,aux=0,suma=0,n=4000000;  // declaramos nuestras variables
+    while(b<=n){ // Mientras b sea menor que n
+        aux=a+b; // el auxiliar almacena la suma de a y b
+        a=b;    // a tomara el valor de b
+        b=aux;    // asignamos el valor de b por el auxuliar
+        if(a%2==0)  // condicion, si a es multiplo de 2 que serian pares
+            suma+=a;   // se sumara ese valor, el cual es par
     }
-    cout<<suma;
+    cout<<suma; // imprimimos el valor en pantalla
 }
 
     
