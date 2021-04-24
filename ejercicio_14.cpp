c0a5418a30360f39dcf8f1950cf619b04ef5d135
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-04-24 18:29:46 -0500

    Update ejercicio_14.cpp

--- ejercicio_14.cpp
+++ ejercicio_14.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 using namespace std;
 
-long long collatz(long long num){
-    long long num_grande=0;
-    while (num != 1){
-        num = (num%2==0)?(num/2):(num*3)+1; 
-        ++num_grande; 
-    }
-    return num_grande;
-}
+long long collatz(long long num); // prototipo de funcion
+
 int  main (){
-    long long num_cadena_larga=0 ;
+    long long num_cadena_larga=0 ; // declaramos nuestras variables
     long long cadena_larga=0 ;
     
-    for (int i=1;i<1000000;++i){
-        if (collatz(i)>cadena_larga){
-            num_cadena_larga=i;
-            cadena_larga=collatz(i);
+    for (int i=1;i<1000000;++i){ // iniciamos nuestro for de 1 a menor que 1000000
+        if (collatz(i)>cadena_larga){ // condicion si mi numero es mayor que cadena_larga
+            num_cadena_larga=i;       // cadena_larga tomara el valor de numero que genera el collatz mas grande
+            cadena_larga=collatz(i);   // agregamos el valor de collat< a cadena_larga para la siguiente pasada
         }
     }
-    cout<<num_cadena_larga;
+    cout<<num_cadena_larga; //imprimimos el valor en pantalla
+    return 0;
+}
+
+long long collatz(long long num){ // implementacion de la funcion
+    long long num_grande=0;    // declaramos nuestras variables
+    while (num != 1){  // mientras  num sea diferente a 1, que es donde terminara el loop
+        num = (num%2==0)?(num/2):(num*3)+1;    //condicional ternario si num es multiplo de 2 
+                                               // se cumple (num/2), sino se cumple (num*3)+1
+        num_grande++;     //por cada ciclo num_grande se ira sumando uno, hasta encontrar la cadena mas larga
+    }
+    return num_grande; // retorna num_grande 
 }
 
