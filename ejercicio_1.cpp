553dd2cbbfc0ae4e080a2934a19da3a8ba55d751
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-04-24 18:26:38 -0500

    Update ejercicio_1.cpp

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int main(){
-    int suma=0;
-	for(int i=0; i<1000; i++){
-		if(i%3==0 || i%5== 0)
-        	suma += i;
+    int suma=0;  // declaramos nuestra variable suma con un valor de 0
+	for(int i=1; i<1000; i++){ // hacemos un for de 0 hasta el numero 999
+		if(i%3==0 || i%5== 0) // condicion si es multiplo de 3 o 5
+        	suma += i;          // si cumple la condicion se suma el numero que fue divisible
     }
-    cout<<"La suma total es: "<<suma;
+    cout<<"La suma total es: "<<suma;  // imprimimos el resultado
     return 0;
 }
 
